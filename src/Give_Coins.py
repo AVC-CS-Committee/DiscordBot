0047497cae5f5c0d85cdffe4401e93522cf472c5
busterblake <33817212+busterblake@users.noreply.github.com>
2023-10-20 14:05:34 -0700

    fixes

---
+++ src/Give_Coins.py
@@ -0,0 +1,5 @@
+from firebase_admin import db, credentials
+
+
+def givecoins(ctx, arg):
+    print("do nothing :)")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 from dotenv import load_dotenv
 import os
 import discord
-#import discord.ext
+# import discord.ext
 from discord.ext import commands
 from discord import app_commands
 import firebase_admin
 from firebase_admin import db, credentials
-
-
 
 # this loads the env file with the API key to be stored
 # locally make sure you have your .env file set
@@ -24,8 +22,9 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!!', intents=intents)
 
-#ref = db.reference("py/")
-#db = firestore.client()
+
+# ref = db.reference("py/")
+# db = firestore.client()
 
 @bot.event
 async def on_ready():
@@ -51,22 +50,33 @@ async def add(ctx, arg, arg2):
 @bot.command()
 async def account(ctx, arg):
     if arg == "join":
+        userid = str(ctx.author.id)
         username = ctx.author.name
-        users_ref = db
-        users_ref.update({
-            username: {
+
+        user_ref = db.child('users').child(userid)
+        user_data = user_ref.get()
+
+        if user_data:   # check for account existence
+            await ctx.send('You already have an account')
+            return
+        else:
+            users_ref = db.child('users')  # Adjust path if needed for database
+            user_path = users_ref.child(userid)
+            user_data = {
+                'Username': username,
                 'Coins': 20,
                 'attendance': 0
             }
-        })
-        await ctx.send(username)
-    elif arg == "info":
-        username = ctx.author.name
-        user_ref = db.child(username)  # Adjust the path as per your database structure
+            user_path.set(user_data)
+            await ctx.send(f"Account created for {username}")
 
+    elif arg == "info":
+        userid = str(ctx.author.id)
+        user_ref = db.child('users').child(userid)  #  Adjust path if needed for database
         user_data = user_ref.get()
 
         if user_data:
+            username = user_data.get('Username')
             coins_value = user_data.get('Coins')
             attendance_value = user_data.get('attendance')
             await ctx.send(f"User: {username}\nCoins: {coins_value}\nAttendance: {attendance_value}")
@@ -76,48 +86,56 @@ async def account(ctx, arg):
         await ctx.send("Not a valid command. Should be 'account join' or 'account info'.")
 
 
+
 @bot.command()
-async def give(ctx, recipient: discord.Member, amount: int):
-    # Get the sender's and recipient's usernames
-    sender_username = ctx.author.name
-    recipient_username = recipient.name
+async def give(ctx, recipient: discord.User, amount: int):
+    try:
+        sender_id = ctx.author.id
+        recipient_id = recipient.id
 
-    # Check if the sender and recipient are the same user
-    if sender_username == recipient_username:
-        await ctx.send("You cannot send coins to yourself.")
-        return
+        if sender_id == recipient_id:
+            await ctx.send("You cannot send coins to yourself.")
+            return
 
-    # Create references to the sender's and recipient's user data
-    sender_ref = db.child(sender_username)
-    recipient_ref = db.child(recipient_username)
+        # Construct paths for the sender and recipient
+        sender_path = f'users/{sender_id}'
+        recipient_path = f'users/{recipient_id}'
 
-    # Check if the sender exists in the database
-    sender_data = sender_ref.get()
-    if sender_data is None:
-        await ctx.send("Sender does not exist in the database.")
-        return
+        sender_ref = db.child(sender_path)
+        recipient_ref = db.child(recipient_path)
 
-    # Check if the recipient exists in the database
-    recipient_data = recipient_ref.get()
-    if recipient_data is None:
-        await ctx.send("Recipient does not exist in the database.")
-        return
+        # Check if the sender and recipient exist in the database
+        sender_data = sender_ref.get()
+        recipient_data = recipient_ref.get()
 
-    # Check if the sender has enough coins to send
-    sender_coins = sender_data.get('Coins')
-    if sender_coins < amount:
-        await ctx.send("You do not have enough coins to send.")
-        return
+        if sender_data is None:
+            await ctx.send("Sender does not exist in the database.")
+            return
 
-    # Update the sender's and recipient's coin balances
-    sender_new_balance = sender_coins - amount
-    recipient_coins = recipient_data.get('Coins')
-    recipient_new_balance = recipient_coins + amount
+        if recipient_data is None:
+            await ctx.send("Recipient does not exist in the database.")
+            return
 
-    sender_ref.update({'Coins': sender_new_balance})
-    recipient_ref.update({'Coins': recipient_new_balance})
+        sender_coins = sender_data.get('Coins')
+        if sender_coins < amount:
+            await ctx.send("You do not have enough coins to send.")
+            return
 
-    await ctx.send(f"{ctx.author.mention} sent {amount} coins to {recipient.mention}.")
+        sender_new_balance = sender_coins - amount
+        recipient_coins = recipient_data.get('Coins')
+        recipient_new_balance = recipient_coins + amount
+
+        sender_ref.update({'Coins': sender_new_balance})
+        recipient_ref.update({'Coins': recipient_new_balance})
+
+        sender_name = ctx.author.name
+        recipient_name = recipient.name
+
+        await ctx.send(f"{sender_name} sent {amount} coins to {recipient_name}.")
+    except ValueError:
+        await ctx.send("Invalid recipient. Please mention a valid user in the format `@username`.")
+
+
 
 @bot.hybrid_command()
 async def test(ctx, arg):
