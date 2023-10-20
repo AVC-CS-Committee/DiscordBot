259603dacd5b3da2ed28ac00c23f480cf4d2ef01
busterblake <33817212+busterblake@users.noreply.github.com>
2023-10-19 23:25:12 -0700

    Fixed added 2 commands give/info

--- src/main.py
+++ src/main.py
@@ -9,41 +9,23 @@ from firebase_admin import db, credentials
 
 
 
-# ref = db.reference("py/")
-# users_ref = ref.child('Users')
-# users_ref.set({
-#     'TestName': {
-#         'Coins': 99,
-#         "attendance": 0
-#     }
-# })
-#
-# ref.get()
-# db.reference("/Username")
-#
-# test_ref = users_ref.child('TestName')
-# test_ref.update({
-#     "Coins": 15
-# })
-# print(ref.get("/coins"))
-
-
 # this loads the env file with the API key to be stored
 # locally make sure you have your .env file set
-
 load_dotenv('.env')
 disc_token: str = os.getenv('DISC_TOKEN')
 firebase_url: str = os.getenv('FIREBASE_URL')
 
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred, {"databaseURL": firebase_url})
+db = firebase_admin.db.reference('/py/users')
 
 intents = discord.Intents.all()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!!', intents=intents)
 
-ref = db.reference("py/")
+#ref = db.reference("py/")
+#db = firestore.client()
 
 @bot.event
 async def on_ready():
@@ -65,25 +47,77 @@ async def add(ctx, arg, arg2):
     arg3 = int(arg) + int(arg2)
     await ctx.send(arg3)
 
+
 @bot.command()
 async def account(ctx, arg):
     if arg == "join":
         username = ctx.author.name
-        users_ref = ref.child('Users')
+        users_ref = db
         users_ref.update({
             username: {
                 'Coins': 20,
-                "attendance": 0
+                'attendance': 0
             }
         })
         await ctx.send(username)
     elif arg == "info":
         username = ctx.author.name
-        ref.child(username)
-        await ctx.send(ref.get("/coins"))
-    else:
-        await ctx.send("Not valid Command should be account join or info")
+        user_ref = db.child(username)  # Adjust the path as per your database structure
 
+        user_data = user_ref.get()
+
+        if user_data:
+            coins_value = user_data.get('Coins')
+            attendance_value = user_data.get('attendance')
+            await ctx.send(f"User: {username}\nCoins: {coins_value}\nAttendance: {attendance_value}")
+        else:
+            await ctx.send("No data found for this user.")
+    else:
+        await ctx.send("Not a valid command. Should be 'account join' or 'account info'.")
+
+
+@bot.command()
+async def give(ctx, recipient: discord.Member, amount: int):
+    # Get the sender's and recipient's usernames
+    sender_username = ctx.author.name
+    recipient_username = recipient.name
+
+    # Check if the sender and recipient are the same user
+    if sender_username == recipient_username:
+        await ctx.send("You cannot send coins to yourself.")
+        return
+
+    # Create references to the sender's and recipient's user data
+    sender_ref = db.child(sender_username)
+    recipient_ref = db.child(recipient_username)
+
+    # Check if the sender exists in the database
+    sender_data = sender_ref.get()
+    if sender_data is None:
+        await ctx.send("Sender does not exist in the database.")
+        return
+
+    # Check if the recipient exists in the database
+    recipient_data = recipient_ref.get()
+    if recipient_data is None:
+        await ctx.send("Recipient does not exist in the database.")
+        return
+
+    # Check if the sender has enough coins to send
+    sender_coins = sender_data.get('Coins')
+    if sender_coins < amount:
+        await ctx.send("You do not have enough coins to send.")
+        return
+
+    # Update the sender's and recipient's coin balances
+    sender_new_balance = sender_coins - amount
+    recipient_coins = recipient_data.get('Coins')
+    recipient_new_balance = recipient_coins + amount
+
+    sender_ref.update({'Coins': sender_new_balance})
+    recipient_ref.update({'Coins': recipient_new_balance})
+
+    await ctx.send(f"{ctx.author.mention} sent {amount} coins to {recipient.mention}.")
 
 @bot.hybrid_command()
 async def test(ctx, arg):
