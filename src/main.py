842223f0b0c66c160daae6b2a685535eca9360ad
MBGooN <mbui2@students.avc.edu>
2024-02-23 18:21:42 -0800

    gitignore

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import discord
+# import sympy as sp    FIX ME: BLOCKED FOR TEMP POLYNOMIAL SOLVER
 #import discord.ext
 from discord.ext import commands
 from discord import app_commands
@@ -16,7 +17,6 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 @bot.event
 async def on_ready():
     print("bot is online")
@@ -26,17 +26,34 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-
 @bot.tree.command(name='hello')
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hello !", ephemeral=False)
-
 
 @bot.command()
 async def add(ctx, arg, arg2):
     arg3 = int(arg) + int(arg2)
     await ctx.send(arg3)
 
+@bot.command()
+async def sub(ctx, arg, arg2):
+    arg3 = int(arg) - int(arg2)
+    await ctx.send(arg3)
+
+####################### FIX ME: BLOCKED TEMP POLYNOMIAL SOLVER
+# @bot.command()
+# async def solve(ctx, equation):
+#    try:
+#        x = sp.symbols('x')
+#       solutions = sp.solve(equation, x)
+#
+#        if solutions:
+#            response = f"Solutions: {solutions}"
+#        else:
+#            response = "No solutions found."
+#        await ctx.send(response)
+#    except Exception as e:
+#        await ctx.send(f"An error occurred: {str(e)}")
 
 @bot.hybrid_command()
 async def test(ctx, arg):
